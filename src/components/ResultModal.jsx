052daab200369628e7597229ba1fe16ec301b1df
Mohiuddin Mohi <mohiuddinr.22@gmail.com>
2025-10-05 07:55:46 +0600

    first commit

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,36 @@
+import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function ResultModal({ref, targetTime, remainingTime, onReset}) {
+    const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+    useImperativeHandle(ref, () => {
+        return {
+            open(){
+                dialog.current.showModal();
+            }
+        };
+    });
+
+    return createPortal(
+        <dialog ref={dialog} className="result-modal">
+            <h2>You {userLost ? `Lost :(` : `Won :)`}</h2>
+            {userLost ? <p>Score: 00</p> : <p>Score: {score}</p>}
+            <p>
+                Your target time was <strong>{targetTime} second{targetTime > 1 ? 's' : ''}</strong>
+            </p>
+            <p>
+                You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong>
+            </p>
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+
+        document.getElementById("modal")
+    );
+}
